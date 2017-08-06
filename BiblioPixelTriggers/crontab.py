d618c5ff61f0a4f7c86106dcbc5a4234e5d28fe5
Adam Haile <adammhaile@gmail.com>
2017-08-06 09:27:35 -0400

    adding solar trigger and update for new config format

--- BiblioPixelTriggers/crontab.py
+++ BiblioPixelTriggers/crontab.py
@@ -5,23 +5,22 @@ import time
 
 
 class crontab(trigger.TriggerBase):
-    def __init__(self, q, configs):
-        super().__init__(q, configs)
-
+    def __init__(self, q, events, **kwargs):
+        super().__init__(q, events)
         self.crontabs = []
-        for c in self.configs:
-            tab = croniter(c['config'], datetime.now())
+        for event in self.events:
+            tab = croniter(event['config'], datetime.now())
             self.crontabs.append({
-                'name': c['name'],
+                'name': event['name'],
                 'tab': tab,
                 'next': tab.get_next(datetime)
             })
 
     def start(self):
         while True:
-            now = datetime.now()
+            now = datetime.now().replace(second=0, microsecond=0, tzinfo=None)
             for c in self.crontabs:
-                if c['next'] <= now:
+                if c['next'] == now:
                     self.trigger(c['name'])
                     c['next'] = c['tab'].get_next(datetime)
-            time.sleep(60 - now.second)
+            time.sleep(60 - datetime.now().second)
